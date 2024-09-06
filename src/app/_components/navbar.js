c61c5fe1b35cae3ba78ebfc4fffdb01633dfd46c
Aidan Mascoli <145822273+denayeisnotokay@users.noreply.github.com>
2024-09-06 17:20:39 -0400

    merge logged in/not logged in site versions for previewing

--- src/app/_components/navbar.js
+++ src/app/_components/navbar.js
@@ -24,8 +24,6 @@ export default function Navbarr() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const pathname = usePathname();
 
-    const loggedIn = true;
-
     return <Navbar isBlurred={false} isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}  maxWidth="full" classNames={{
         base: ['fixed', 'bg-transparent', 'font-heading'],
         menu: ['fixed', 'inset-0', 'pt-16', 'px-0', 'gap-0'],
@@ -51,54 +49,26 @@ export default function Navbarr() {
                     Create Meeting
                 </Link>
             </NavbarItem>
-            { loggedIn ? <>
-                <NavbarItem className="hidden sm:flex" isActive={pathname === "/availability"}>
-                    <Link href="/availability" className="text-default-foreground">
-                        My Availability
-                    </Link>
-                </NavbarItem>
-                <NavbarItem className="hidden sm:flex" isActive={pathname === "/meetings"}>
-                    <Link href="/meetings" className="text-default-foreground">
-                        My Meetings
-                    </Link>
-                </NavbarItem>
-                <Dropdown onOpenChange={setIsDropdownOpen}>
-                    <NavbarItem className={'flex justify-end hover:bg-transparent'}>
-                        <DropdownTrigger>
-                            <Button
-                                color="sky"
-                                disableRipple
-                                className="bg-transparent !inline-flex !flex-row h-full text-md"
-                                startContent={<IoMdPerson />}
-                                endContent={<IoIosArrowDown className={'transition-transform' + (isDropdownOpen ? ' rotate-180' : '')} />}
-                                radius="sm"
-                                variant="light"
-                            >
-                                My Account
-                            </Button>
-                        </DropdownTrigger>
-                    </NavbarItem>
-                    <DropdownMenu
-                        color="default"
-                        aria-label="Account Options"
-                    >
-                        <DropdownItem key="settings" href={'/settings'}>Account Settings</DropdownItem>
-                        <DropdownItem key="inbox" href={'/inbox'}>Inbox</DropdownItem>
-                        <DropdownItem color="danger" key="logout" onClick={() => {}} className={'text-danger'}>Log Out</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </> : <>
-                <NavbarItem className="hidden sm:flex" isActive={pathname === "/login"}>
-                    <Button as={Link} variant="light" color="primary" href="/login" className="text-md">
-                        Log In
-                    </Button>
-                </NavbarItem>
-                <NavbarItem className="hidden sm:flex" isActive={pathname === "/signup"}>
-                    <Button as={Link} color="primary" href="/signup" className="text-md">
-                        Create Account
-                    </Button>
-                </NavbarItem>
-            </> }
+            <NavbarItem className="hidden sm:flex" isActive={pathname === "/availability"}>
+                <Link href="/availability" className="text-default-foreground">
+                    My Availability
+                </Link>
+            </NavbarItem>
+            <NavbarItem className="hidden sm:flex" isActive={pathname === "/meetings"}>
+                <Link href="/meetings" className="text-default-foreground">
+                    My Meetings
+                </Link>
+            </NavbarItem>
+            <NavbarItem className="hidden sm:flex" isActive={pathname === "/login"}>
+                <Button as={Link} variant="light" color="primary" href="/login" className="text-md">
+                    Log In
+                </Button>
+            </NavbarItem>
+            <NavbarItem className="hidden sm:flex" isActive={pathname === "/signup"}>
+                <Button as={Link} color="primary" href="/signup" className="text-md">
+                    Create Account
+                </Button>
+            </NavbarItem>
         </NavbarContent>
         <NavbarMenu motionProps={{animate: { height: isMenuOpen ? '100vh' : '0' }}}>
             <NavbarMenuItem isActive={pathname === "/"}>
@@ -117,52 +87,40 @@ export default function Navbarr() {
                     Create Meeting
                 </Link>
             </NavbarMenuItem>
-            { loggedIn ? <>
-                <NavbarMenuItem isActive={pathname === "/settings"}>
-                    <Link
-                        className="w-full text-default-foreground"
-                        href="/settings"
-                    >
-                        Account Settings
-                    </Link>
-                </NavbarMenuItem>
-                <NavbarMenuItem isActive={pathname === "/inbox"}>
-                    <Link
-                        className="w-full text-default-foreground"
-                        href="/inbox"
-                    >
-                        Inbox
-                    </Link>
-                </NavbarMenuItem>
-                <NavbarMenuItem>
-                    <Link
-                        color="danger"
-                        className="w-full"
-                        onClick={() => {}}
-                    >
-                        Log Out
-                    </Link>
-                </NavbarMenuItem>
-            </> : <>
-                <NavbarMenuItem isActive={pathname === "/login"}>
-                    <Link
-                        color="secondary"
-                        className="w-full"
-                        href="/login"
-                    >
-                        Log In
-                    </Link>
-                </NavbarMenuItem>
-                <NavbarMenuItem isActive={pathname === "/signup"}>
-                    <Link
-                        color="primary"
-                        className="w-full"
-                        href="/signup"
-                    >
-                        Create Account
-                    </Link>
-                </NavbarMenuItem>
-            </> }
+            <NavbarMenuItem isActive={pathname === "/availability"}>
+                <Link
+                    className="w-full text-default-foreground"
+                    href="/availability"
+                >
+                    My Availability
+                </Link>
+            </NavbarMenuItem>
+            <NavbarMenuItem isActive={pathname === "/meetings"}>
+                <Link
+                    className="w-full text-default-foreground"
+                    href="/meetings"
+                >
+                    My Meetings
+                </Link>
+            </NavbarMenuItem>
+            <NavbarMenuItem isActive={pathname === "/login"}>
+                <Link
+                    color="secondary"
+                    className="w-full"
+                    href="/login"
+                >
+                    Log In
+                </Link>
+            </NavbarMenuItem>
+            <NavbarMenuItem isActive={pathname === "/signup"}>
+                <Link
+                    color="primary"
+                    className="w-full"
+                    href="/signup"
+                >
+                    Create Account
+                </Link>
+            </NavbarMenuItem>
         </NavbarMenu>
     </Navbar>
 }
